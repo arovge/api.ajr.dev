055acb4128d441f63a33fad4f049c8def7f0cae0
Austin Rovge <austinrovge@gmail.com>
2022-07-10 23:29:39 -0500

    break files operations out into files service

--- src/filesController.ts
+++ src/filesController.ts
@@ -1,5 +1,6 @@
 import { Env } from ".";
 import common from "./common"
+import filesService from "./filesService";
 
 export default (
     url: URL,
@@ -8,55 +9,21 @@ export default (
 ): Promise<Response> => {
     const key = url.pathname.replace('/files/', '');
     if (request.method.toLowerCase() === 'get') {
-        return getFile(key, env);
+        return filesService.getFile(key, env);
     }
     if (request.method.toLowerCase() === 'put') {
         if (common.isAuthorized(request, env)) {
-            return putFile(key, env, request);
+            return filesService.putFile(key, env, request);
         } else {
             return common.forbidden();
         }
     }
     if (request.method.toLowerCase() === 'delete') {
         if (common.isAuthorized(request, env)) {
-            return deleteFile(key, env);
+            return filesService.deleteFile(key, env);
         } else {
             return common.forbidden();
         }
     }
     return Promise.resolve(common.notFound());
 }
-
-const getFile = async (
-    key: string,
-    env: Env
-): Promise<Response> => {
-    const file = await env.FILES_BUCKET.get(key);
-
-    if (file === null) {
-        return common.notFound();
-    }
-
-    const headers = new Headers();
-    file.writeHttpMetadata(headers);
-    headers.set('etag', file.httpEtag);
-
-    return new Response(file.body, { headers });
-};
-
-const putFile = async (
-    key: string,
-    env: Env,
-    request: Request
-): Promise<Response> => {
-    await env.FILES_BUCKET.put(key, request.body);
-    return common.ok();
-};
-
-const deleteFile = async (
-    key: string,
-    env: Env
-): Promise<Response> => {
-    await env.FILES_BUCKET.delete(key);
-    return common.ok();
-};
